347a2eadef1a9f0125964a4ca2e18e225d1e990b
pfm-vond <pierre.vonderscher@gmail.com>
2021-04-30 18:34:04 +0200

    Update Templates Solution to include a small configuration sample code in the console

--- Templates/Net5/ConsoleApp/Program.cs
+++ Templates/Net5/ConsoleApp/Program.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Program
+    public record Config();
+
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            IConfiguration host = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddJsonFile("setting.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            host.GetSection("Config").Get<Config>();
+
+            await Task.CompletedTask;
         }
     }
 }
