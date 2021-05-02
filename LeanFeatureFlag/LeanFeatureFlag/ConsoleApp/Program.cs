a71b0621221e0e4a670101e3d94d27f32abc3c24
pfm-vond <pierre.vonderscher@gmail.com>
2021-05-02 11:25:29 +0200

    create some snippet to avoid to loose time and remove the config part of the project template because we have a snippet for that now :)

--- LeanFeatureFlag/LeanFeatureFlag/ConsoleApp/Program.cs
+++ LeanFeatureFlag/LeanFeatureFlag/ConsoleApp/Program.cs
@@ -3,20 +3,10 @@ using System.Threading.Tasks;
 
 namespace LeanFeatureFlag.ConsoleApp
 {
-    public record Config();
-
     public class Program
     {
         static async Task Main(string[] args)
         {
-            IConfiguration cfg = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddJsonFile("settings.json")
-                .AddEnvironmentVariables()
-                .Build();
-
-            cfg.GetSection("Config").Get<Config>();
-
             await Task.CompletedTask;
         }
     }
